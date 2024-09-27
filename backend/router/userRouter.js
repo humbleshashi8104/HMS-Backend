b975840061d2a2e87a8d6c328bc396981e98ab4a
Shashi Chaubey <kumarshashi8104@gmail.com>
2024-09-28 02:54:13 +0530

    logoutfixed

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -23,7 +23,7 @@ router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
 router.get("/doctors", getAllDoctors);
 router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/admin/me", isAdminAuthenticated, getUserDetails);
-router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
+router.get("/patient/logout", logoutPatient);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
 
 export default router;
